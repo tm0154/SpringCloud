17ff0762709d92da0c9f7eec7dee8bb42c5934fb
tieminPan <job@tmpan.cn>
2020-03-13 15:42:00 +0800

    添加gateway网关服务

---
+++ initialization-gateway/src/main/java/cn/tmpan/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package cn.tmpan.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author tieMinPan
+ * @Desc: 网关服务
+ * @date 2020-03-13 15:25
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
